7d1ccc7d88c24489f8897eea7892e5a9438a67d1
yassun <yassun910@gmail.com>
2021-07-11 10:30:00 +0900

    add startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
+use todoapp::configuration::get_configuration;
+use todoapp::startup::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
