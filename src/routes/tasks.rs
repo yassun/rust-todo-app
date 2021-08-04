7c118bed692e61df609c9c042fdb9683e565cd95
yassun <yassun910@gmail.com>
2021-08-04 09:28:50 +0900

    add task domain

--- src/routes/tasks.rs
+++ src/routes/tasks.rs
@@ -1,6 +1,13 @@
 use actix_web::{web, HttpResponse};
+use crate::domain::{Todo};
 
 pub async fn tasks(id: web::Path<u32>) -> HttpResponse {
     println!("get_tasks id:{}", id);
-    HttpResponse::Ok().finish()
+
+    let id_option: Option<u32> = Some(id.into_inner());
+    HttpResponse::Ok().json(Todo {
+        id: id_option,
+        content: String::from("TODO"),
+        done: false,
+    })
 }
