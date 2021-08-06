d481ad800428e74922b6c54a7dfa3df67972df4b
yassun <yassun910@gmail.com>
2021-08-06 09:43:07 +0900

    add create task

--- src/routes/add_task.rs
+++ src/routes/add_task.rs
@@ -1,7 +1,33 @@
 use actix_web::{web, HttpResponse};
 use crate::domain::{Todo};
+use sqlx::postgres::PgPool;
+use uuid::Uuid;
 
-pub async fn add_task(task: web::Json<Todo>) -> HttpResponse {
+pub async fn add_task(
+    task: web::Json<Todo>,
+    pool: web::Data<PgPool>
+) -> Result<HttpResponse, HttpResponse> {
     println!("add_task #{:?}", task);
-    HttpResponse::Ok().finish()
+    insert_task(&pool, &task)
+    .await
+    .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+pub async fn insert_task(pool: &PgPool, task: &Todo) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+    INSERT INTO tasks (id, content, done)
+    VALUES ($1, $2, $3)
+            "#,
+        Uuid::new_v4(),
+        task.content,
+        task.done
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        eprintln!("Failed to execute query: {:?}", e);
+    })?;
+    Ok(())
 }
